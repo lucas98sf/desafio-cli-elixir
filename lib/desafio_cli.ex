ca45bbc4feb4fac7d9c87b836e33e0f8d1d29663
Lucas Ferreira <lucas.98sf@gmail.com>
2024-09-05 00:15:56 -0300

    feat: add genserver

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -3,19 +3,18 @@ defmodule DesafioCli do
   Ponto de entrada para a CLI.
   """
 
-  defmodule State do
-    defstruct database: %{}, transactions: []
-  end
+  alias DesafioCli.Database
 
   @doc """
   A função main recebe os argumentos passados na linha de
   comando como lista de strings e executa a CLI.
   """
   def main(_args) do
-    loop(%State{})
+    {:ok, pid} = Database.start_link()
+    loop(pid)
   end
 
-  defp loop(state) do
+  defp loop(pid) do
     IO.write("> ")
 
     case IO.read(:line) do
@@ -23,61 +22,155 @@ defmodule DesafioCli do
         :ok
 
       input ->
-        {result, new_state} =
+        result =
           input
           |> String.trim()
-          |> String.split()
-          |> handle_command(state)
+          |> StringUtils.split_with_quotes()
+          |> handle_command(pid)
 
         case result do
           {:ok, message} -> IO.puts(message)
           {:error, message} -> IO.puts("ERR #{message}")
         end
 
-        loop(new_state)
+        loop(pid)
     end
   end
 
-  def handle_command(["BEGIN"], state) do
-    new_state = %{state | transactions: [%{} | state.transactions]}
-    {{:ok, length(new_state.transactions)}, new_state}
-  end
-
-  def handle_command(["ROLLBACK"], state) do
-    case state.transactions do
-      [] ->
-        {{:error, "No transaction in progress"}, state}
-
-      [_ | rest] ->
-        new_state = %{state | transactions: rest}
-        {{:ok, length(new_state.transactions)}, new_state}
+  def handle_command([command | args], pid) do
+    case String.upcase(command) do
+      "BEGIN" -> handle_begin(args, pid)
+      "ROLLBACK" -> handle_rollback(args, pid)
+      "COMMIT" -> handle_commit(args, pid)
+      "GET" -> handle_get(args, pid)
+      "SET" -> handle_set(args, pid)
+      _ -> {:error, "No command #{command}"}
     end
   end
 
-  def handle_command(["COMMIT"], state) do
-    case state.transactions do
-      [] ->
-        {{:error, "No transaction in progress"}, state}
+  defp handle_begin([], pid), do: Database.begin(pid)
+  defp handle_begin(_, _pid), do: {:error, "BEGIN - Syntax error"}
 
-      transactions ->
-        merged = Enum.reduce(transactions, %{}, &Map.merge/2)
-        new_state = %{state | database: Map.merge(state.database, merged), transactions: []}
-        {{:ok, 0}, new_state}
+  defp handle_rollback([], pid), do: Database.rollback(pid)
+  defp handle_rollback(_, _pid), do: {:error, "ROLLBACK - Syntax error"}
+
+  defp handle_commit([], pid), do: Database.commit(pid)
+  defp handle_commit(_, _pid), do: {:error, "COMMIT - Syntax error"}
+
+  defp handle_get([key], pid), do: Database.get(pid, key)
+  defp handle_get(_, _pid), do: {:error, "GET <key> - Syntax error"}
+
+  defp handle_set([key, value], pid) do
+    parsed_value = parse_value(value)
+    Database.set(pid, key, parsed_value)
+  end
+
+  defp handle_set(_, _pid), do: {:error, "SET <key> <value> - Syntax error"}
+
+  defp parse_value(arg) do
+    cond do
+      String.match?(arg, ~r/^-?\d+$/) -> String.to_integer(arg)
+      String.match?(arg, ~r/^TRUE$/i) -> true
+      String.match?(arg, ~r/^FALSE$/i) -> false
+      true -> arg
+    end
+  end
+end
+
+defmodule DesafioCli.Database do
+  @moduledoc """
+  Controla o estado da database e as transações
+  """
+
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, %{database: %{}, transactions: []})
+  end
+
+  def begin(pid) do
+    GenServer.call(pid, :begin)
+  end
+
+  def rollback(pid) do
+    GenServer.call(pid, :rollback)
+  end
+
+  def commit(pid) do
+    GenServer.call(pid, :commit)
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
+  def set(pid, key, value) do
+    GenServer.call(pid, {:set, key, value})
+  end
+
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call(:begin, _from, state) do
+    new_state = Map.put(state, :transactions, state.transactions ++ [%{}])
+    {:reply, {:ok, length(new_state.transactions)}, new_state}
+  end
+
+  @impl true
+  def handle_call(:rollback, _from, state) do
+    case length(state.transactions) do
+      0 ->
+        {:reply, {:error, "No transaction in progress"}, state}
+
+      _ ->
+        new_state = Map.put(state, :transactions, List.delete_at(state.transactions, -1))
+        {:reply, {:ok, length(new_state.transactions)}, new_state}
     end
   end
 
-  def handle_command(["GET", key], state) do
+  @impl true
+  def handle_call(:commit, _from, state) do
+    case length(state.transactions) do
+      0 ->
+        {:reply, {:error, "No transaction in progress"}, state}
+
+      1 ->
+        new_state = %{
+          transactions: [],
+          database: Map.merge(state.database, List.first(state.transactions))
+        }
+
+        {:reply, {:ok, 0}, new_state}
+
+      _ ->
+        last_transaction = List.last(state.transactions)
+        remaining_transactions = List.delete_at(state.transactions, -1)
+
+        new_state =
+          Map.put(
+            state,
+            :transactions,
+            List.delete_at(remaining_transactions, -1) ++
+              [Map.merge(List.last(remaining_transactions), last_transaction)]
+          )
+
+        {:reply, {:ok, length(new_state.transactions)}, new_state}
+    end
+  end
+
+  @impl true
+  def handle_call({:get, key}, _from, state) do
     value = get_value(state, key)
-    {{:ok, value}, state}
+    {:reply, {:ok, value}, state}
   end
 
-  def handle_command(["SET", key, value], state) do
+  @impl true
+  def handle_call({:set, key, value}, _from, state) do
     {new_state, exists} = set_value(state, key, value)
-    {{:ok, "#{exists} #{value}"}, new_state}
-  end
-
-  def handle_command([command | _], state) do
-    {{:error, "No command #{command}"}, state}
+    {:reply, {:ok, "#{exists} #{value}"}, new_state}
   end
 
   defp get_value(state, key) do
@@ -90,18 +183,26 @@ defmodule DesafioCli do
     exists = if Map.has_key?(merged_state, key), do: "TRUE", else: "FALSE"
 
     new_state =
-      case state.transactions do
-        [] ->
-          %{state | database: Map.put(state.database, key, value)}
+      if Enum.empty?(state.transactions) do
+        Map.put(state, :database, Map.put(state.database, key, value))
+      else
+        last_transaction = List.last(state.transactions)
 
-        [current | rest] ->
-          %{state | transactions: [Map.put(current, key, value) | rest]}
+        Map.put(
+          state,
+          :transactions,
+          List.delete_at(state.transactions, -1) ++ [Map.put(last_transaction, key, value)]
+        )
       end
 
     {new_state, exists}
   end
 
   defp get_merged_state(state) do
-    Enum.reduce(state.transactions, state.database, &Map.merge/2)
+    transactions_merged =
+      state.transactions
+      |> Enum.reduce(%{}, &Map.merge(&2, &1))
+
+    Map.merge(state.database, transactions_merged)
   end
 end
--- test/desafio_cli_test.exs
+++ test/desafio_cli_test.exs
@@ -1,72 +1,124 @@
-defmodule DesafioCliTest do
+defmodule DesafioCli.DatabaseTest do
+  use ExUnit.Case, async: true
+  alias DesafioCli.Database
+
+  setup do
+    {:ok, pid} = Database.start_link()
+    %{pid: pid}
+  end
+
+  describe "begin/1" do
+    test "starts a new transaction", %{pid: pid} do
+      assert {:ok, 1} = Database.begin(pid)
+      assert {:ok, 2} = Database.begin(pid)
+    end
+  end
+
+  describe "rollback/1" do
+    test "rolls back the last transaction", %{pid: pid} do
+      Database.begin(pid)
+      assert {:ok, 0} = Database.rollback(pid)
+    end
+
+    test "returns error when no transaction is in progress", %{pid: pid} do
+      assert {:error, "No transaction in progress"} = Database.rollback(pid)
+    end
+  end
+
+  describe "commit/1" do
+    test "commits the last transaction", %{pid: pid} do
+      Database.begin(pid)
+      assert {:ok, 0} = Database.commit(pid)
+    end
+
+    test "merges nested transactions", %{pid: pid} do
+      Database.begin(pid)
+      Database.set(pid, "x", 1)
+      Database.begin(pid)
+      Database.set(pid, "x", 2)
+      assert {:ok, 1} = Database.commit(pid)
+      assert {:ok, 2} = Database.get(pid, "x")
+    end
+
+    test "returns error when no transaction is in progress", %{pid: pid} do
+      assert {:error, "No transaction in progress"} = Database.commit(pid)
+    end
+  end
+
+  describe "get/2" do
+    test "retrieves a value", %{pid: pid} do
+      Database.set(pid, "x", 42)
+      assert {:ok, 42} = Database.get(pid, "x")
+    end
+
+    test "returns NIL for non-existent key", %{pid: pid} do
+      assert {:ok, "NIL"} = Database.get(pid, "nonexistent")
+    end
+
+    test "retrieves value from ongoing transaction", %{pid: pid} do
+      Database.begin(pid)
+      Database.set(pid, "x", 42)
+      assert {:ok, 42} = Database.get(pid, "x")
+    end
+  end
+
+  describe "set/3" do
+    test "sets a value", %{pid: pid} do
+      assert {:ok, "FALSE 42"} = Database.set(pid, "x", 42)
+      assert {:ok, 42} = Database.get(pid, "x")
+    end
+
+    test "overwrites an existing value", %{pid: pid} do
+      Database.set(pid, "x", 42)
+      assert {:ok, "TRUE 43"} = Database.set(pid, "x", 43)
+      assert {:ok, 43} = Database.get(pid, "x")
+    end
+
+    test "sets a value in ongoing transaction", %{pid: pid} do
+      Database.begin(pid)
+      Database.set(pid, "x", 42)
+      Database.rollback(pid)
+      assert {:ok, "NIL"} = Database.get(pid, "x")
+    end
+  end
+end
+
+defmodule DesafioCli.CLITest do
   use ExUnit.Case
   import ExUnit.CaptureIO
+  alias DesafioCli.Database
 
   describe "handle_command/2" do
-    test "BEGIN command" do
-      state = %DesafioCli.State{}
-
-      assert {{:ok, 1}, %DesafioCli.State{transactions: [%{}]}} =
-               DesafioCli.handle_command(["BEGIN"], state)
+    setup do
+      {:ok, pid} = Database.start_link()
+      %{pid: pid}
     end
 
-    test "ROLLBACK command" do
-      state = %DesafioCli.State{transactions: [%{}]}
-
-      assert {{:ok, 0}, %DesafioCli.State{transactions: []}} =
-               DesafioCli.handle_command(["ROLLBACK"], state)
+    test "BEGIN command", %{pid: pid} do
+      assert {:ok, 1} = DesafioCli.handle_command(["BEGIN"], pid)
     end
 
-    test "ROLLBACK command with no transaction" do
-      state = %DesafioCli.State{}
-
-      assert {{:error, "No transaction in progress"}, ^state} =
-               DesafioCli.handle_command(["ROLLBACK"], state)
+    test "ROLLBACK command", %{pid: pid} do
+      DesafioCli.handle_command(["BEGIN"], pid)
+      assert {:ok, 0} = DesafioCli.handle_command(["ROLLBACK"], pid)
     end
 
-    test "COMMIT command" do
-      state = %DesafioCli.State{transactions: [%{"x" => 1}]}
-
-      assert {{:ok, 0}, %DesafioCli.State{database: %{"x" => 1}, transactions: []}} =
-               DesafioCli.handle_command(["COMMIT"], state)
+    test "COMMIT command", %{pid: pid} do
+      DesafioCli.handle_command(["BEGIN"], pid)
+      assert {:ok, 0} = DesafioCli.handle_command(["COMMIT"], pid)
     end
 
-    test "COMMIT command with no transaction" do
-      state = %DesafioCli.State{}
-
-      assert {{:error, "No transaction in progress"}, ^state} =
-               DesafioCli.handle_command(["COMMIT"], state)
+    test "GET command", %{pid: pid} do
+      DesafioCli.handle_command(["SET", "x", "42"], pid)
+      assert {:ok, 42} = DesafioCli.handle_command(["GET", "x"], pid)
     end
 
-    test "GET command" do
-      state = %DesafioCli.State{database: %{"x" => 1}}
-      assert {{:ok, 1}, ^state} = DesafioCli.handle_command(["GET", "x"], state)
+    test "SET command", %{pid: pid} do
+      assert {:ok, "FALSE 42"} = DesafioCli.handle_command(["SET", "x", "42"], pid)
     end
 
-    test "GET command for non-existent key" do
-      state = %DesafioCli.State{}
-      assert {{:ok, "NIL"}, ^state} = DesafioCli.handle_command(["GET", "x"], state)
-    end
-
-    test "SET command" do
-      state = %DesafioCli.State{}
-
-      assert {{:ok, "FALSE 1"}, %DesafioCli.State{database: %{"x" => "1"}}} =
-               DesafioCli.handle_command(["SET", "x", "1"], state)
-    end
-
-    test "SET command in transaction" do
-      state = %DesafioCli.State{transactions: [%{}]}
-
-      assert {{:ok, "FALSE 1"}, %DesafioCli.State{transactions: [%{"x" => "1"}]}} =
-               DesafioCli.handle_command(["SET", "x", "1"], state)
-    end
-
-    test "Invalid command" do
-      state = %DesafioCli.State{}
-
-      assert {{:error, "No command INVALID"}, ^state} =
-               DesafioCli.handle_command(["INVALID"], state)
+    test "Invalid command", %{pid: pid} do
+      assert {:error, "No command INVALID"} = DesafioCli.handle_command(["INVALID"], pid)
     end
   end
 
